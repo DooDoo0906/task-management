a865d988340ef65b66bbf7d0bb33a78cc96f942e
Lai Dinh Thuan (Jack) <laidinhthuan123@gmail.com>
2023-12-06 21:50:48 +0700

    style active link on navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,24 +1,29 @@
+"use client";
+import classNames from "classnames";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { FaTasks } from "react-icons/fa";
 
 const NavBar = () => {
+  const currentPathname = usePathname();
   const links = [
     { lable: "Dashboard", link: "/" },
-    { lable: "Tasks", link: "/" },
+    { lable: "Tasks", link: "/tasks" },
   ];
   return (
-    <nav className="flex space-x-6 mb-6 border-b px-5 h-14 items-center">
-      <Link
-        href="/"
-        className="hover:text-zinc-200 hover:text-zinc-200 transition-colors"
-      >
+    <nav className="flex space-x-8 mb-6 border-b px-5 h-14 items-center">
+      <Link href="/" className="hover:text-zinc-200 transition-colors">
         <FaTasks />
       </Link>
       <ul className="flex space-x-6">
         {links.map((item, index) => (
           <Link
-            className="text-zinc-500 hover:text-zinc-200 transition-colors"
+            className={classNames({
+              "text-zinc-100": item.link === currentPathname,
+              "text-zinc-500": item.link !== currentPathname,
+              "hover:text-zinc-300 transition-colors": true,
+            })}
             key={index}
             href={item.link}
           >
