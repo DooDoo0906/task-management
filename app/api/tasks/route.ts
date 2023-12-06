6145747544979fbce89b8663d3618450a1c194df
Lai Dinh Thuan (Jack) <laidinhthuan123@gmail.com>
2023-12-06 23:31:48 +0700

    Build API for creating issue

---
+++ app/api/tasks/route.ts
@@ -0,0 +1,18 @@
+import { TaskSchema } from "@/app/schema/TaskSchema";
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "../../../prisma/client"
+
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+    const validation = TaskSchema.safeParse(body);
+    if (!validation.success) {
+        return NextResponse.json(validation.error.errors, { status: 400 })
+    }
+    const newTask = await prisma.task.create({
+        data: {
+            title: body.title,
+            description: body.description,
+        }
+    })
+    return NextResponse.json(newTask, { status: 201 })
+}
